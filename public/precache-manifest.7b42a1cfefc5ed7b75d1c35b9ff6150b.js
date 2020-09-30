1a86baec61d09a57a22433ffeb2d513f88849469
pedroobando <pedroobando@hotmail.com>
2020-09-30 08:04:55 -0400

    app finalizada

---
+++ public/precache-manifest.7b42a1cfefc5ed7b75d1c35b9ff6150b.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "42667a152d8793089e44357b6b28594d",
+    "url": "/index.html"
+  },
+  {
+    "revision": "eaa6b1ffcd30ce0a84c8",
+    "url": "/static/css/2.ceee16bc.chunk.css"
+  },
+  {
+    "revision": "f8550bb3360ec70eaeea",
+    "url": "/static/css/main.00a8eb23.chunk.css"
+  },
+  {
+    "revision": "eaa6b1ffcd30ce0a84c8",
+    "url": "/static/js/2.fc5db812.chunk.js"
+  },
+  {
+    "revision": "590af14d9bb9b4b1926797b2a32e33a1",
+    "url": "/static/js/2.fc5db812.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "f8550bb3360ec70eaeea",
+    "url": "/static/js/main.2e87fe39.chunk.js"
+  },
+  {
+    "revision": "7e829d7f4f2411c25d99",
+    "url": "/static/js/runtime-main.8c2230a5.js"
+  }
+]);
